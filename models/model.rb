e27198f9deca4df43a806749ad8bc2c2c68655e5
ricardojrt6565 <ricardojrt6565@hstat.org>
2017-03-08 18:28:57 +0000

    first push

---
+++ models/model.rb
@@ -0,0 +1,13 @@
+# https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+require 'net/http'
+require 'json'
+require 'pp' 
+
+url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+result = JSON.parse(response)
+  puts result
+  
+# Has to return temp num and unit
+
