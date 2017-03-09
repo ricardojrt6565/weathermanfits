f672a79ffb27525a9b3a5e573c8ab82a1c2a805a
ricardojrt6565 <ricardojrt6565@hstat.org>
2017-03-09 20:11:20 +0000

    add correct url

--- models/model.rb
+++ models/model.rb
@@ -1,13 +1,13 @@
-# https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+# https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 require 'net/http'
 require 'json'
 require 'pp' 
 
-url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 result = JSON.parse(response)
- pp result["query"]["results"]["channel"]["ttl"]
+ pp result["query"]["results"]["channel"]["item"]["condition"]["temp"]
   
 # Say for example i'm trying to get the "humidity"
 
